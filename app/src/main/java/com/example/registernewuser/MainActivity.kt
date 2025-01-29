0f6fa4e6ebaa47a76e2fc071667fb8f664636a41
e1349462 <e1349462@u.nus.edu>
2025-01-29 18:24:04 +0800

    Merged login project into loginActivity branch

---
+++ app/src/main/java/com/example/registernewuser/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.registernewuser
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login) // Use login.xml as the main layout
+    }
+}
