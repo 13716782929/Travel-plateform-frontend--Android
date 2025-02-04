332fc44dcb60281cc6613e486d4fefeb1042a82e
e1349462 <e1349462@u.nus.edu>
2025-02-04 16:43:26 +0800

    Updated logicof RegisterActivity.kt and include some gradle stuff and edit layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.registeruser"
     compileSdk = 35
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.registeruser"
         minSdk = 29
--- app/src/main/java/com/example/registeruser/RegisterActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.registeruser;
-
-import android.app.Activity;
-
-public class RegisterActivity extends Activity {
-}
