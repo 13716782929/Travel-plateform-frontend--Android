cbdd401b460b8c795115da65871f65d9777eae39
Aki Chang <wangchang0409@163.com>
2025-02-06 13:34:21 +0800

    update 2-6 13:34

--- app/src/main/java/iss/nus/edu/sg/mygo/fragment/SearchFragment.kt
+++ app/src/main/java/iss/nus/edu/sg/mygo/fragment/SearchFragment.kt
@@ -1,17 +1,34 @@
 package iss.nus.edu.sg.mygo.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import iss.nus.edu.sg.mygo.R
+import iss.nus.edu.sg.mygo.home.HotelMainActivity
 
 class SearchFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.search_fragment, container, false)
+        // Inflate the layout for this fragment
+        val binding = inflater.inflate(R.layout.search_fragment, container, false)
+
+        // 获取 TextView 组件
+        val hotelsCategoryText: TextView = binding.findViewById(R.id.search_hotels_category_txt)
+
+        // 设置点击监听器
+        hotelsCategoryText.setOnClickListener {
+            // 跳转到 HotelMainActivity
+            val intent = Intent(requireContext(), HotelMainActivity::class.java)
+            startActivity(intent)
+        }
+
+        return binding
     }
 }
