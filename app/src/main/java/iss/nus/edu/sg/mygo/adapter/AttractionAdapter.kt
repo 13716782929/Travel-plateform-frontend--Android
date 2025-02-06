f0a64462568f96852a6eb3bf634b7947723b3334
Aki Chang <wangchang0409@163.com>
2025-02-07 00:48:24 +0800

    homepage api service update

--- app/src/main/java/iss/nus/edu/sg/mygo/adapter/AttractionAdapter.kt
+++ app/src/main/java/iss/nus/edu/sg/mygo/adapter/AttractionAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import iss.nus.edu.sg.mygo.R
 import iss.nus.edu.sg.mygo.models.Attraction
 
@@ -27,14 +28,28 @@ class AttractionAdapter(
 
     override fun onBindViewHolder(holder: AttractionViewHolder, position: Int) {
         val attraction = attractions[position]
-        Log.d("AttractionAdapter", "Binding attraction: ${attraction.attractionName}")
-        holder.nameTextView.text = attraction.attractionName
-        holder.locationTextView.text = attraction.location
-        holder.imageView.setImageResource(attraction.imageResId)
+        Log.d("AttractionAdapter", "Binding attraction: ${attraction.name}")
+
+        holder.nameTextView.text = attraction.name
+        holder.locationTextView.text = attraction.address
+//        holder.priceTextView.text = attraction.price
+
+        // 使用 Glide 加载图片 use Glide to load image
+        if (attraction.imageUrls.isNotEmpty()) {
+            Glide.with(holder.itemView.context)
+                .load(attraction.imageUrls[0]) // 只加载第一张图片
+                .placeholder(R.drawable.attraction_placeholder_image) // 加载中显示的占位图
+                .error(R.drawable.attraction_placeholder_image) // 失败时显示的图片
+                .into(holder.imageView)
+        } else {
+            holder.imageView.setImageResource(R.drawable.attraction_placeholder_image) // 没有图片时使用默认占位图
+        }
+
         holder.itemView.setOnClickListener {
             onItemClick(position)
         }
     }
+
 
     override fun getItemCount(): Int {
         return attractions.size
@@ -44,6 +59,8 @@ class AttractionAdapter(
     class AttractionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val nameTextView: TextView = itemView.findViewById(R.id.attraction_name)
         val locationTextView: TextView = itemView.findViewById(R.id.attraction_location)
+//        val priceTextView: TextView = itemView.findViewById(R.id.pax)
         val imageView: ImageView = itemView.findViewById(R.id.attraction_image)
     }
+
 }
