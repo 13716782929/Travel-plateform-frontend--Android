1e6220c6dd280195932ecc47a375528532070ee6
Aki Chang <wangchang0409@163.com>
2025-02-07 02:39:08 +0800

    homepage api service update

--- app/src/main/java/iss/nus/edu/sg/mygo/models/Attraction.kt
+++ app/src/main/java/iss/nus/edu/sg/mygo/models/Attraction.kt
@@ -5,12 +5,13 @@ import iss.nus.edu.sg.mygo.enum.TicketType
 data class Attraction(
     val uuid: String,
     val name: String,
-    val address: String,              // 格式化后的地址（例如 "1 Example St, Singapore 123456"）
-    val latitude: Double?,            // 纬度
-    val longitude: Double?,           // 经度
+    val address: String,
+    val latitude: Double?,
+    val longitude: Double?,
     val description: String,
-    val price: String,                // 格式化之后的价格
-    val openTime: String,             // 格式化后的开放时间（如 "Daily: 00:00-23:59"）
-    val ticketAvailability: Boolean,  // 是否有票
-    var imageUrls: List<String>       // 改为存储网络图片的 URL 列表
+    val price: String,
+    val openTime: String,
+    val ticketAvailability: Boolean,
+    var imageUuid: String // ✅ 只存 UUID，不存 URL
 )
+
