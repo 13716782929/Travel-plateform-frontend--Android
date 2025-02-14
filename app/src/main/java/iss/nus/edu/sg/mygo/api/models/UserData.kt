cb863440f751d891e4a490d4ab1fe658dbcd6d8e
Aki Chang <wangchang0409@163.com>
2025-02-14 13:36:45 +0800

    fix debug

--- app/src/main/java/iss/nus/edu/sg/mygo/api/models/UserData.kt
+++ app/src/main/java/iss/nus/edu/sg/mygo/api/models/UserData.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 // the model to request data from API
 data class RegisterRequest(
     @SerializedName ("email") val email: String,
-    @SerializedName ("passwprd") val password: String,
+    @SerializedName ("password") val password: String,
     @SerializedName ("preference") val preference: String?
 )
 data class LoginRequest(
@@ -14,7 +14,7 @@ data class LoginRequest(
 )
 
 data class LoginResponse(
-    @SerializedName("token") val token: String,
+    @SerializedName ("token") val token: String,
     @SerializedName ("userId") val userId: String,
     @SerializedName ("name") val name: String,
     @SerializedName ("email") val email: String,
